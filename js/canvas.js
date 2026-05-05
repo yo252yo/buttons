2136d3d494fb332a6535aca9926ddc43b556cd95
yo252yo <yo252yo@gmail.com>
2026-05-05 03:12:58 +0200

    children spawn from left to right i think

--- js/canvas.js
+++ js/canvas.js
@@ -15,11 +15,24 @@ export async function loadButtonsFromCanvas() {
     buttonsCache[id] = Button.fromCanvasNode(node, []);
   }
 
+  const nodePositions = {};
+  for (const node of canvas.nodes) {
+    nodePositions[node.id] = node.x || 0;
+  }
+
   for (const edge of canvas.edges) {
     const from = edge.fromNode;
     const to = edge.toNode;
     if (from && to && buttonsCache[from]) {
       buttonsCache[from].children.push(to);
+    }
+  }
+
+  // Sort children by x position of child nodes (left to right)
+  for (const id in buttonsCache) {
+    const btn = buttonsCache[id];
+    if (btn.children.length > 0) {
+      btn.children.sort((a, b) => -1 * (nodePositions[a] - nodePositions[b]));
     }
   }
 
