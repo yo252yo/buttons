2ec9646c54ba97a00ae60506ab7f27e91440e2f7
yo252yo <yo252yo@gmail.com>
2026-05-05 21:56:57 +0200

    add a example gamer mode css

--- pages/settings.js
+++ pages/settings.js
@@ -1,8 +1,28 @@
+const gamerModeStyle = document.getElementById('gamer_mode_style');
+let gamerMode = gamerModeStyle.textContent !== '';
+
 const pageDiv = document.getElementById('page_div');
 pageDiv.innerHTML = `
 <h1>Settings</h1>
+<button id="gamer_mode_btn" class="button button-grey${gamerMode ? ' pressed' : ''}">Gamer Mode: ${gamerMode ? 'ON' : 'OFF'}</button>
+<br /><br />
 TBC WIP<br />
 Should have an option to dedupe duplicates, autoscroll, keyboard control<br />
 Also control the content of the interface bar.
 
 `;
+
+document.getElementById('gamer_mode_btn').onclick = () => {
+  gamerMode = !gamerMode;
+  const btn = document.getElementById('gamer_mode_btn');
+  if (gamerMode) {
+    btn.classList.add('pressed');
+    btn.textContent = 'Gamer Mode: ON';
+  } else {
+    btn.classList.remove('pressed');
+    btn.textContent = 'Gamer Mode: OFF';
+  }
+  gamerModeStyle.textContent = gamerMode 
+    ? '#buttons_zone .button-orange { color: var(--orange); } #buttons_zone .button-orange.pressed { color: var(--orange-border); } #buttons_zone .button-purple { color: var(--purple); } #buttons_zone .button-purple.pressed { color: var(--purple-border); } #buttons_zone .button-blue { color: var(--blue); } #buttons_zone .button-blue.pressed { color: var(--blue-border); } #buttons_zone .button-green { color: var(--green); } #buttons_zone .button-green.pressed { color: var(--green-border); } #buttons_zone .button-grey { color: var(--grey); } #buttons_zone .button-grey.pressed { color: var(--grey-border); }'
+    : '#buttons_zone .button { color: var(--black); }';
+};
